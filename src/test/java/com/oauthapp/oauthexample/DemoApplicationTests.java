cb8e85e5d7a58332ececadedd753fb7bb7442d26
cwood89 <cwood89@mac.com>
2020-07-28 07:56:09 -0400

    added dependencies

---
+++ src/test/java/com/oauthapp/oauthexample/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthapp.oauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
