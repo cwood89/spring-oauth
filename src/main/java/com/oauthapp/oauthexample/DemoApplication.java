59ff05061979f08cfd6360b8074ff81049ca3f08
cwood89 <cwood89@mac.com>
2020-07-28 08:49:49 -0400

    made homepage public

--- src/main/java/com/oauthapp/oauthexample/DemoApplication.java
+++ src/main/java/com/oauthapp/oauthexample/DemoApplication.java
@@ -5,14 +5,18 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication extends WebSecurityConfigurerAdapter {
 
 	@GetMapping("/user")
 	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
@@ -21,6 +25,21 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+			http
+					.authorizeRequests(a -> a
+							.antMatchers("/", "/error", "/webjars/**").permitAll()
+							.anyRequest().authenticated()
+					)
+					.exceptionHandling(e -> e
+							.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+					)
+					.oauth2Login();
+			// @formatter:on
 	}
 
 }
